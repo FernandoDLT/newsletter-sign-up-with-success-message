1664b23046669a58f990e828c6df56ab7b6d46a0
FernandoDLT <96254499+FernandoDLT@users.noreply.github.com>
2023-05-29 18:31:37 -0600

    Fixed dismiss message button, media q., hover on dismisss button

--- script.js
+++ script.js
@@ -1,48 +1,68 @@
-const containerEl = document.getElementById('container')
-const leftEl = document.getElementById('left')
-const rightEl = document.getElementById('right')
+// const form = document.querySelector("form");
+// const card_1 = document.querySelector(".card");
+// const card_2 = document.querySelector(".card-2");
+// form.addEventListener("submit", (e) => {
+//    e.preventDefault();
+//    card_1.classList.add("hide");
+//    card_2.classList.remove("hide")
+// });
 
-const userEmailEl = document.getElementById('user-email')
-const invalidEmailEl = document.getElementById('invalid-email')
-const emailInput = document.getElementById('email')
-const submitBtnEl = document.getElementById('submit-btn')
+// function validateEmail(email) {
+//    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+//    return emailRegex.test(email)
+// }
 
+// const form = document.querySelector("form");
+// const card_1 = document.querySelector(".card");
+// const card_2 = document.querySelector(".card-2");
+// const errorMessage = document.querySelector(".error-message");
 
-const confirmedMessageEl = document.getElementById('confirmed-message')
-const dismissMessageEl = document.getElementById('dismiss-message')
+// form.addEventListener("submit", (e) => {
+//    e.preventDefault();
+//    const emailInput = document.querySelector("#email");
+//    const email = emailInput.value.trim();
+   
+//    if (!validateEmail(email)) {
+//       errorMessage.textContent = "Valid email required";
+//       errorMessage.style.display = "block";
+//    } else {
+//       card_1.classList.add("hide");
+//       card_2.classList.remove("hide");
+//    }
+// });
 
-function formSuccess() {
-   confirmedMessageEl.classList.add('active')
-   containerEl.classList.add('success')
-   leftEl.style.display = 'none'
-   rightEl.style.display = 'none'
-}
+// function validateEmail(email) {
+//    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+//    return emailRegex.test(email);
+// }
+
+const form = document.querySelector("form");
+const card_1 = document.querySelector(".card");
+const card_2 = document.querySelector(".card-2");
+const errorMessage = document.querySelector(".error-message");
+const dismissButton = document.querySelector(".dismiss");
+
+form.addEventListener("submit", (e) => {
+   e.preventDefault();
+   const emailInput = document.querySelector("#email");
+   const email = emailInput.value.trim();
+   
+   if (!validateEmail(email)) {
+      errorMessage.textContent = "Valid email required";
+      errorMessage.style.display = "block";
+   } else {
+      card_1.classList.add("hide");
+      card_2.classList.remove("hide");
+   }
+});
+
+dismissButton.addEventListener("click", () => {
+   window.location.href = "index.html";
+   e.preventDefault();
+});
 
 function validateEmail(email) {
-   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
-   return emailRegex.test(email)
+   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+   return emailRegex.test(email);
 }
 
-submitBtnEl.addEventListener('click', (e) => {
-   e.preventDefault()
-   const email = emailInput.value.trim()
-
-   if (validateEmail(email)) {
-      formSuccess()
-      userEmailEl.innerText = email
-      emailInput.value = ''
-
-      invalidEmailEl.classList.remove('active')
-      emailInput.classList.remove('active')
-   } else {
-      invalidEmailEl.classList.add('active')
-      emailInput.classList.add('active')
-   }
-})
-
-dismissMessageEl.addEventListener('click', () => {
-   leftEl.style.display = 'block'
-   rightEl.style.display = 'block'
-   containerEl.classList.remove('success')
-   confirmedMessageEl.classList.remove('active')
-})
